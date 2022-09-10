36d0723d6d861e1098196beccceede4dc9b3cc33
nikolas <nikolasps7@gmail.com>
2022-09-10 12:50:42 -0300

    alguns testes a mais e mudei nome de test.py para testApplication

---
+++ testApplication.py
@@ -0,0 +1,38 @@
+import unittest
+from src.application.EventService import  EventService
+from src.data.Event import Event
+from src.data.Address import Address
+from src.data.User import User
+from src.application.EventConverter import EventConverter
+from datetime import date, time
+
+
+class TestEvent(unittest.TestCase):
+    #exemplo só
+    def test_create_event(self):
+        
+        user = User('nibs','nikolasps7@gmail.com','senha123')
+        user.set_id(166)
+        event_service = EventService()
+        Post = {'name': 'nibs event', 'start_date': '2022-09-17', 'end_date': '2022-09-22', 'check_in': '22:44', 'check_out': '00:44', 'visibility': 'public', 'street': 'Mario Leitao', 'house_number': '60', 'zip-code': '9069425', 'state': 'RS', 'city': 'Porto Alegre'}
+        Post['host'] = user
+        converter = EventConverter()
+        event = converter.input_to_event_object(Post)
+        self.assertIsInstance(event_service.save(event), Event) # verifica se o retorno é do tipo Event
+    def test_get_events(self):
+
+        event_service = EventService()
+        events = event_service.get_events()
+        #[print(event.name) for event in events]
+        self.assertIsInstance(events, list) # verifica se o retorno é uma lista
+        self.assertIsInstance(events[0], Event) # verifica se o retorno é uma lista de eventos
+
+    def test_get_events_by_name(self):
+        name = 'nibs' 
+        event_service = EventService()
+        events = event_service.get_events_by_name(name)
+        [print(event.name) for event in events]
+        self.assertIsInstance(events, list) # verifica se o retorno é uma lista
+        self.assertIsInstance(events[0], Event) # verifica se o retorno é uma lista de eventos
+if __name__ == '__main__':
+    unittest.main()
---
+++ testDAO.py
@@ -0,0 +1,70 @@
+import unittest
+from src.data.dao.AddressDao import AddressDao
+from src.data.dao.DBConnection import DBConnectionSingleton
+
+
+
+class TestAddress(unittest.TestCase):
+    def test_query_by_street(self):
+
+        db_connection  = DBConnectionSingleton.get_instance()
+        #order of tuple = (address_id, street, house_number, city, event_state, zip_code,apartment,complement)
+        address_dao = AddressDao(db_connection)
+        
+        addresses = address_dao.get_adresses_by_street('Mario Leitao')
+        regex = "Mario Leitao/*"
+        for address_tuple in addresses:
+            self.assertRegex(address_tuple[1],regex) #1 is street
+            print(address_tuple[1])
+        addresses = address_dao.get_adresses_by_street('sa')
+        regex = "sa/*"
+        for address_tuple in addresses:
+            self.assertRegex(address_tuple[1],regex) #1 is street
+            print(address_tuple[1])
+        DBConnectionSingleton.destroyer()
+
+    def test_query_by_state(self):
+        db_connection  = DBConnectionSingleton.get_instance()
+        #order of tuple = (address_id, street, house_number, city, event_state, zip_code,apartment,complement)
+        address_dao = AddressDao(db_connection)
+        
+        addresses = address_dao.get_adresses_by_state('Rio Grande do Sul')
+        regex = "Rio Grande do Sul/*"
+        for address_tuple in addresses:
+            self.assertRegex(address_tuple[4],regex) #4 is state
+            print(address_tuple[4])
+        addresses = address_dao.get_adresses_by_state('rs')
+        regex = "rs/*"
+        for address_tuple in addresses:
+            self.assertRegex(address_tuple[4],regex) #4 is state
+        DBConnectionSingleton.destroyer()
+
+    def test_query_by_city(self):
+        db_connection  = DBConnectionSingleton.get_instance()
+        #order of tuple = (address_id, street, house_number, city, event_state, zip_code,apartment,complement)
+        address_dao = AddressDao(db_connection)
+        
+        addresses = address_dao.get_adresses_by_city('Porto Alegre')
+        regex = "Porto Alegre/*"
+        for address_tuple in addresses:
+            self.assertRegex(address_tuple[3],regex) #3 is city
+            print(address_tuple[3])
+        addresses = address_dao.get_adresses_by_city('POA')
+        regex = "POA/*"
+        for address_tuple in addresses:
+            self.assertRegex(address_tuple[3],regex) #3 is city
+            print(address_tuple[3])
+        DBConnectionSingleton.destroyer()
+
+    def test_query_by_zip(self):
+        db_connection  = DBConnectionSingleton.get_instance()
+        #order of tuple = (address_id, street, house_number, city, event_state, zip_code,apartment,complement)
+        address_dao = AddressDao(db_connection)
+        
+        addresses = address_dao.get_adresses_by_zip_code(90690425)
+        for address_tuple in addresses:
+            self.assertEqual(address_tuple[5],90690425) #5 is zip
+            print(address_tuple[5])
+        DBConnectionSingleton.destroyer()
+if __name__ == '__main__':
+    unittest.main()
