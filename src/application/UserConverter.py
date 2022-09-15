1f65158371ba524566694a744da11d07ee8e44b2
nikolas <nikolasps7@gmail.com>
2022-09-15 20:55:42 -0300

    converte dict pra obj, vice-versa entre outros

---
+++ src/application/UserConverter.py
@@ -0,0 +1,23 @@
+
+from dataclasses import asdict
+from src.data.User import User
+
+class UserConverter:
+
+    def __init__(self) -> None:
+        pass
+
+    #TODO maybe encrypt password
+    def dict_to_object(self,dict):
+        return User(**dict)
+
+    def object_to_dict(self,user:User):
+        return asdict(user)
+
+    def database_tuple_to_object(self,user_tuple):
+        keys = ('name','email','password')
+        input_data = dict(zip(keys,user_tuple[1:])) #dont take id field, set later
+        user = User(**input_data)
+        user.set_id(user_tuple[0]) #event_id is in tuple[0], then its host,name....
+        return user
+
