1250c25f672dc51b31db92105978758af137f7e0
nikolas <nikolasps7@gmail.com>
2022-09-15 20:55:05 -0300

    login, ainda não testado

--- src/application/UserService.py
+++ src/application/UserService.py
@@ -7,27 +7,32 @@ class UserService:
     
     def __init__(self) -> None:
         pass 
-    #TODO maybe pass an User as parameter, dontknow. And maybe encrypt password
-    def create_user(self,name,email,password):
+    
+    def create_user(self,user:User):
         #regex for email
         #shorter version of the RFC 5322-compliant Regular Expression 
         regex = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
-        if  re.match(regex,email) is None:
+        if  re.match(regex,user.get_email()) is None:
             raise Exception("Email not valid")
-        
         db_connection  = DBConnectionSingleton.get_instance()
         user_dao = UserDao(db_connection)
         try:
-            user_dao.insert_user(User(name,email,password))
+            user_dao.insert_user(user)
         except Exception as e:
             #probably user already exist
             raise(e)
-        finally:
-            DBConnectionSingleton.destroyer()
 
-        return User(name,email,password)
+        return user
     
-    def login(self,name,email,password):
-
-        pass
+    def login(self,email,password):
+        db_connection  = DBConnectionSingleton.get_instance()
+        user_dao = UserDao(db_connection)
+        user = user_dao.get_user_by_email(email)
+        #TODO maybe raise one unique exception for email and password (more security)
+        #TODO transform user in User object before doing logic to it
+        if user is None:
+            raise Exception("Invalid Email")
+        if user[2] != password: #user[2] its the password on db
+            raise Exception("Invalid Password")
+        return user
     
