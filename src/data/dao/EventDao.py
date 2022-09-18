06ed999a1abf00e9696ffc16780b93b8187f8da8
nikolas <nikolasps7@gmail.com>
2022-09-18 02:18:43 -0300

    agora pega dados da tabela de participants tb

--- src/data/dao/EventDao.py
+++ src/data/dao/EventDao.py
@@ -10,31 +10,70 @@ class EventDao:
         self.__cur = connection.get_cursor()
     
     def insert_event(self,event:Event):
-        inser_script = "INSERT INTO Events (event_id, host_id, event_name, address_id, start_date, end_date, visibility, check_in, check_out, event_parent_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        """
+        insert event into database
+        if there is participants in this event object, insert them into Participants TABLE
+        """
+        insert_script = "INSERT INTO Events (event_id, host_id, event_name, address_id, start_date, end_date, visibility, check_in, check_out, event_parent_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         if event.get_event_parent() is None:
             event_parent_id = None
         else:
             event_parent_id = event.get_event_parent().get_id()
         insert_value = (event.get_id(), event.get_host().get_id(), event.get_name(), event.get_address().get_id(),event.get_start_date(), event.get_end_date(), event.get_visibility(), event.get_check_in(), event.get_check_out(),event_parent_id)
-        self.__cur.execute(inser_script, insert_value)
+        self.__cur.execute(insert_script, insert_value)
+        
+        insert_participants_script = "INSERT INTO Participants (user_id,event_id) VALUES (%s, %s)"
+        for participant in event.get_list_of_participants():
+            insert_participant_value = (participant.get_id(),event.get_id())
+            self.__cur.execute(insert_participants_script, insert_participant_value)
         self.__conn.commit()
         return event.get_id()
         
     def print_all_events(self):
+        """
+        print all events, but dont print participants of the event
+        """
         query_scrpit = "SELECT * FROM Events"
         self.__cur.execute(query_scrpit)
-        participants = []
+        participants_dict = {}
+        
         for record in self.__cur.fetchall():
-            participants.append(record[10])
+            if participants_dict[record[0]] is None:
+                participants = []
+            participants.append(record[10]) #record[10] = user_id
+            participants_dict[record[0]] = participants
             print(record)
         self.__conn.commit()
         
     def get_all_events(self):
+        """
+        DB returns tuples like:
+        (01,1234,'event name',9069042560, '2023-06-03','2023-06-03',TRUE,'20:06:03','23:06:03',NULL,1234);
+        (01,1234,'event name',9069042560, '2023-06-03','2023-06-03',TRUE,'20:06:03','23:06:03',NULL,1236);
+        (01,1234,'event name',9069042560, '2023-06-03','2023-06-03',TRUE,'20:06:03','23:06:03',NULL,1235);
+        (04,1234,'event name',9069042560, '2023-06-03','2023-06-03',TRUE,'20:06:03','23:06:03',NULL,1238);
+
+        i.e there is multiple tuples of the same event, just varying the user participant
+        """
         query_scrpit = "SELECT * FROM Events JOIN Participants using (event_id) ORDER BY event_id"
         self.__cur.execute(query_scrpit)
+        participants_dict = {}
+        records = self.__cur.fetchall()
+        #populate participants dict, each key is an event_id, each value is a list of participants of that event
+        for record in records:
+            if record[0] not in participants_dict:
+                participants_dict[record[0]] = []
+            participants_dict[record[0]].append(record[10])
+        
+        event_id_lst = []
         events_lst = []
-        for record in self.__cur.fetchall():
-            events_lst.append(record)
+        for record in records:    
+            if record[0] in event_id_lst:
+                continue
+            else:
+                event_id_lst.append(record[0])
+                events_lst.append((*record[:10],participants_dict[record[0]])) #record tuple without user_id + list of participants
+
         self.__conn.commit()
         return events_lst
 
@@ -48,24 +87,63 @@ class EventDao:
         pass
 
     def get_events_by_name(self,name):
-        query_scrpit = "SELECT * FROM Events WHERE event_name LIKE %s"
+        """
+        DB returns tuples like:
+        (01,1234,'event name',9069042560, '2023-06-03','2023-06-03',TRUE,'20:06:03','23:06:03',NULL,1234);
+        (01,1234,'event name',9069042560, '2023-06-03','2023-06-03',TRUE,'20:06:03','23:06:03',NULL,1236);
+        (01,1234,'event name',9069042560, '2023-06-03','2023-06-03',TRUE,'20:06:03','23:06:03',NULL,1235);
+        (04,1234,'event name',9069042560, '2023-06-03','2023-06-03',TRUE,'20:06:03','23:06:03',NULL,1238);
+
+        i.e there is multiple tuples of the same event, just varying the user participant
+        """
+        query_scrpit = "SELECT * FROM Events JOIN Participants using (event_id) WHERE event_name LIKE %s ORDER BY event_id"
         name = '%' + name + '%'#get event names that have this name as prefix, suffix or in the middle of the event name
         input = (name,)
         self.__cur.execute(query_scrpit,input)
         events_lst = []
-        for record in self.__cur.fetchall():
-            events_lst.append(record)
+        records = self.__cur.fetchall()
+        participants_dict = {}
+        #populate participants dict, each key is an event_id, each value is a list of participants of that event
+        for record in records:
+            if record[0] not in participants_dict:
+                participants_dict[record[0]] = []
+            participants_dict[record[0]].append(record[10])
+        
+        event_id_lst = []
+        events_lst = []
+        #create new event tuples, adding list of participants
+        for record in records:    
+            if record[0] in event_id_lst:
+                continue
+            else:
+                event_id_lst.append(record[0])
+                events_lst.append((*record[:10],participants_dict[record[0]])) #record tuple without user_id + list of participants
         self.__conn.commit()
         return events_lst
         
     def get_events_by_street(self,state):
-        query_scrpit = "SELECT * FROM Events WHERE event_name LIKE %s"
+        query_scrpit = "SELECT * FROM Events JOIN Participants using (event_id) WHERE event_name LIKE %s ORDER BY event_id"
         name = '%' + name + '%'#get event names that have this name as prefix, suffix or in the middle of the event name
         input = (name,)
         self.__cur.execute(query_scrpit,input)
         events_lst = []
-        for record in self.__cur.fetchall():
-            events_lst.append(record)
+        records = self.__cur.fetchall()
+        participants_dict = {}
+        #populate participants dict, each key is an event_id, each value is a list of participants of that event
+        for record in records:
+            if record[0] not in participants_dict:
+                participants_dict[record[0]] = []
+            participants_dict[record[0]].append(record[10])
+        
+        event_id_lst = []
+        events_lst = []
+        #create new event tuples, adding list of participants
+        for record in records:    
+            if record[0] in event_id_lst:
+                continue
+            else:
+                event_id_lst.append(record[0])
+                events_lst.append((*record[:10],participants_dict[record[0]])) #record tuple without user_id + list of participants
         self.__conn.commit()
         return events_lst
 
