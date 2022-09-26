287273d426e432ee7a2a853ec76591850d4038f7
Thiago Santos <49589136+ThiagoSzz@users.noreply.github.com>
2022-09-26 14:31:03 -0300

    Add files via upload

--- application.py
+++ application.py
@@ -1,16 +1,26 @@
 import os
-
-dir = os.getcwd()
-
 from flask import Flask, render_template
 
 app = Flask(import_name=__name__,
-            static_folder=dir + r"\images")
+            static_folder=os.getcwd() + r"\images")
+
+username = 'Gustavo'
 
 @app.route("/")
 def index():
-    username = 'Thiago'
-    return render_template('main.html', username=username)
+    return render_template('index.html', username=username)
+
+@app.route("/login")
+def login():
+    return render_template('login.html', username=username)
+
+@app.route("/registro")
+def registro():
+    return render_template('registro.html', username=username)
+
+@app.route("/create_event")
+def create_event():
+    return render_template('create_event.html', username=username)
 
 if __name__ == "__main__":
     app.run()
