06b105c762dbc605157f101d664ddee6a2a3fa0c
Guilherme Torres Marques Vidal <guilherme_torres@hotmail.com.br>
2022-09-27 20:54:26 -0300

    Rename index as home

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, url_for, redirect, render_template
 
 app = Flask(import_name=__name__,
             static_folder=os.getcwd() + r"\images")
@@ -8,20 +8,24 @@ username = 'Gustavo'
 
 @app.route("/")
 @app.route("/home")
-def index():
-    return render_template('index.html', title = 'Gerenciador de eventos', username=username)
+def home():
+    return render_template('home.html', title = 'Gerenciador de eventos', username=username)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template('login.html', username=username)
 
-@app.route("/registro")
+@app.route("/registro", methods=['GET', 'POST'])
 def registro():
     return render_template('registro.html', username=username)
 
-@app.route("/create_event")
+@app.route("/create_event", methods=['GET', 'POST'])
 def create_event():
     return render_template('create_event.html', username=username)
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run()
