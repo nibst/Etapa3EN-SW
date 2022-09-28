10a6b147b53cb78511369221ae4573880bd563ad
gustavo auad piccoli <gustavo.a.piccoli@gmail.com>
2022-09-28 16:22:36 -0300

    Funcao QR implementada

---
+++ src/application/QR_Code.py
@@ -0,0 +1,7 @@
+#EH PRECISO INTALAR pip install qrcode PARA UTILIZAR DESSA BIBLIOTECA
+import qrcode
+
+#Essa funcao recebe um link e um nome de arquivo e gera um arquivo .png com o qr code e o nome passado, no diretorio QR
+def cria_qr(link, nome_arquivo):
+    img = qrcode.make(link)
+    img.save('././QR/'+ nome_arquivo)
