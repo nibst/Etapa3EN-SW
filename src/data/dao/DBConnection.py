67d8f05007fc42af2e8e254f91ac124de57233eb
Thiago Santos <49589136+ThiagoSzz@users.noreply.github.com>
2022-10-04 02:05:21 -0300

    Add files via upload

--- src/data/dao/DBConnection.py
+++ src/data/dao/DBConnection.py
@@ -6,7 +6,7 @@ class DBConnectionSingleton:
     def __init__(self):
         if DBConnectionSingleton.__instance is None:
             #TODO connect parameters in a config file
-            self.__conn =  psycopg2.connect(dbname='event_manager', user='postgres', password='123', host='localhost', port='5432')
+            self.__conn =  psycopg2.connect(dbname='event_manager', user='postgres', password='th1235', host='localhost', port='5433')
             self.__cur = self.__conn.cursor()     
             DBConnectionSingleton.__instance = self
         else:
